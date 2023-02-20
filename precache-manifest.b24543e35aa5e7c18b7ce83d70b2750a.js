24fd5f15eda4d36f6fa7eb9b12c027939f1034e2
DoAnhThang <anhthang1807@gmail.com>
2023-02-20 12:38:39 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b24543e35aa5e7c18b7ce83d70b2750a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f6a1f004872df35335eb3c0a4c025b",
+    "url": "/movieApp/index.html"
+  },
+  {
+    "revision": "cc71f4f2ecf84f831f19",
+    "url": "/movieApp/static/css/main.f7bf006c.chunk.css"
+  },
+  {
+    "revision": "cd5bba132953a7652afe",
+    "url": "/movieApp/static/js/2.746472b1.chunk.js"
+  },
+  {
+    "revision": "c1f0f4da96b8067f4f0dfc4881d93bf3",
+    "url": "/movieApp/static/js/2.746472b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc71f4f2ecf84f831f19",
+    "url": "/movieApp/static/js/main.58fbef63.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/movieApp/static/js/main.58fbef63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670664e239e7648d7426",
+    "url": "/movieApp/static/js/runtime-main.68ca1411.js"
+  }
+]);
